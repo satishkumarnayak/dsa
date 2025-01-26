eb3f1332b3321b112023adc23e8e9f596bb7d4ce
Satish Nayak <satish_nayak@epam.com>
2025-01-25 20:05:45 -0500

    dsa

---
+++ src/arrays/ValidAnagram.java
@@ -0,0 +1,34 @@
+package arrays;
+
+
+// https://leetcode.com/problems/valid-anagram/
+public class ValidAnagram {
+
+  
+  public boolean isAnagram(String s, String t) {
+
+    if(s == null || t == null || s.length() != t.length())
+        return false;
+
+   boolean res = true;
+    int[] sArr = new int[26];
+
+    for(int i = 0; i < s.length(); i++){
+        int ascii = s.charAt(i) - 97 ;
+        sArr[ascii]++;
+    }
+    
+    for(int j = 0; j < t.length(); j++){
+       
+        if(sArr[ (int)t.charAt(j) - 97] < 1 ){
+            res = false;
+            break;
+        } else {                
+            sArr[ (int)t.charAt(j) - 97]--;
+            
+        }
+    }
+      
+    return res;
+}
+}
